cd9b354d7d3f21101254a1437a31911bcaf6a711
chamakuripavani <158549727+chamakuripavani@users.noreply.github.com>
2024-03-15 12:46:13 +0530

    Create class3.java

---
+++ class3.java
@@ -0,0 +1,16 @@
+package javapratice;
+
+public class variabledemo2 {
+
+	public static void main(String[] args) {
+System.out.println(5);
+System.out.println(15);
+System.out.println(15+5);
+System.out.println(15+20);
+int a =20;
+System.out.println(a);
+	System.out.println(a+15);
+	System.out.println(a+20);
+	}
+
+}
